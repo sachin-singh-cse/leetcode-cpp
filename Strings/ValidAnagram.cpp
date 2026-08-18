286ba40cfb9fd13fe435ac5cd92581c4de959c80
sachin-singh-cse <sachin-singh-cse@users.noreply.github.com>
2026-08-18 22:21:56 +0530

    Added Valid Anagram Solutionn

---
+++ Strings/ValidAnagram.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t){
+        if ( s.size() != t.size()) return false;
+
+        vector<int> count(26,0);
+
+        for ( char c : s){
+            count[c-'a']++;
+        }
+        for ( char c : t){
+            count[c-'a']--;
+        }
+
+        bool allZeroes = all_of(begin(count), end(count), [] (int val){
+            return val == 0;
+        });
+
+        return allZeroes;
+    }
+};
+
+int main() {
+
+    string s = "anagram";
+    string t = "nagaram";
+
+    Solution S;
+    cout<<boolalpha<<S.isAnagram(s,t);
+    
+    return 0;
+}
