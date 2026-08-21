8645e0d6f169cefa2ac8a9637d306f4256ebe8b7
sachin-singh-cse <sachin-singh-cse@users.noreply.github.com>
2026-08-21 16:28:14 +0530

    Added Longest Common Prefix Solution

---
+++ Strings/Longest-Common-Prefix.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& str){
+        if ( str.empty()) return "";
+
+        int n = str[0].size();
+        int m = str.size();
+
+        for (int i = 0; i < n; i++) {
+            char c = str[0][i];
+            for (int j = 1; j < m; j++) {
+                if ( i >= str[j].length() || str[j][i] != c){
+                    return str[0].substr(0,i);
+                }
+            }
+        }
+
+        return str[0];
+    }
+};
+
+int main() {
+    vector<string> str = {"flower","flow","flight"};
+
+    Solution S;
+    cout<<S.longestCommonPrefix(str);
+    
+    return 0;
+}
